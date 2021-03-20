5c8f216e1c3b6222820ceba2fbc5de5959c18c5a
Maksym Kotsiuk <m.kotsiuk@gmail.com>
2021-03-20 16:18:01 +0200

    reviews section

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,13 @@ import menu from './components/_menu';
 import tabSliders from './components/_tabSliders';
 import dropdowns from './components/_dropdowns';
 import accordions from './components/_accordions';
+import reviewsSliders from './components/_reviewsSliders';
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
   menu();
+  reviewsSliders();
   tabSliders();
   dropdowns();
   accordions();
---
+++ src/js/components/_reviewsSliders.js
@@ -0,0 +1,43 @@
+import {Swiper, Pagination} from 'swiper';
+
+import { forEach } from '../utils';
+
+Swiper.use([Pagination]);
+
+const sliders = document.querySelectorAll('.js-reviews-slider');
+
+
+const initSlider = el => {
+  const slider = el.querySelector('.js-reviews-slider-el');
+  const nav = slider.querySelector('.js-reviews-slider-nav');
+
+  const swiper = new Swiper(slider, {
+    init: false,
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 10,
+    touchEventsTarget: 'wrapper',
+    pagination: {
+      el: nav,
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      600: {
+        spaceBetween: 20
+      }
+    }
+  });
+
+  swiper.init();
+
+
+};
+
+const reviewsSlider = () => {
+  forEach.call(sliders, el => {
+    initSlider(el);
+  });
+};
+
+export default reviewsSlider;
